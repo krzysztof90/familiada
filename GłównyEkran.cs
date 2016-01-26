e63df4810065fb66e56c487767e45edfb548073f
Krzysztof Szulc <krzysztof_s@ymail.com>
2016-01-26 19:58:56 +0100

    klasa znak

--- GłównyEkran.cs
+++ GłównyEkran.cs
@@ -31,53 +31,27 @@ namespace familiada
 			ComponentResourceManager resources = new ComponentResourceManager(typeof(GłównyEkran));
 
 			TableLayoutPanel cyfryPanel = new TableLayoutPanel();
-			TableLayoutPanel[] cyfry = new TableLayoutPanel[3];
+			Znak[] cyfry = new Znak[3];
 
-			for (int i = 0; i < 3; i++)
+			for(int i=0; i<3; i++)
 			{
-				cyfry[i] = new TableLayoutPanel();
-				cyfry[i].ColumnCount = 5;
-				cyfry[i].ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-				cyfry[i].ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-				cyfry[i].ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-				cyfry[i].ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-				cyfry[i].ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-				cyfry[i].Dock = System.Windows.Forms.DockStyle.Fill;
-				cyfry[i].RowCount = 7;
-				cyfry[i].RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-				cyfry[i].RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
-				cyfry[i].RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
-				cyfry[i].RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
-				cyfry[i].RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
-				cyfry[i].RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
-				cyfry[i].RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28572F));
-
-				for (int kolumna = 0; kolumna < 5; kolumna++)
-					for (int rząd = 0; rząd < 7; rząd++)
-					{
-						PictureBox obraz = new PictureBox();
-						((ISupportInitialize)(obraz)).BeginInit();
-						cyfry[i].Controls.Add(obraz, kolumna, rząd);
-						obraz.Dock = DockStyle.Fill;
-						//obraz.Image = ((Image)(resources.GetObject("pictureBox1.Image")));
-						obraz.Image = global::familiada.Properties.Resources.żółty;
-						((ISupportInitialize)(obraz)).EndInit();
-					}
-
-				cyfryPanel.Controls.Add(cyfry[i]);
+				cyfry[i] = new Znak(30, 2);
+				cyfry[i].dodajDo(cyfryPanel, i*2, 0);
 			}
 
-			cyfryPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
-			cyfryPanel.ColumnCount = 3;
-			cyfryPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-			cyfryPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-			cyfryPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-			cyfryPanel.Location = new System.Drawing.Point(12, 62);
+			//cyfryPanel.BackColor = Color.Black;
+			cyfryPanel.ColumnCount = 5;
+			cyfryPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+			cyfryPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 13.33333F));
+			cyfryPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+			cyfryPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 13.33333F));
+			cyfryPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+			cyfryPanel.Location = new Point(12, 300);
 			cyfryPanel.RowCount = 1;
-			cyfryPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			cyfryPanel.Size = new System.Drawing.Size(190, 100);
+			cyfryPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			cyfryPanel.Size = new Size(500, 300);
 
-			dodatkowy.Controls.Add(cyfryPanel);
+			panel1.Controls.Add(cyfryPanel);
 
 		}
 	}
---
+++ Znak.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace familiada
+{
+	class Znak
+	{
+		int liczbaKolumn;
+		int liczbaRzędów;
+
+		//Panel help = new Panel();
+
+		TableLayoutPanel panel = new TableLayoutPanel();
+		PictureBox[,] piksele;
+
+		public Znak(int liczbaKolumn, int liczbaRzędów)
+		{
+			this.liczbaKolumn = liczbaKolumn;
+			this.liczbaRzędów = liczbaRzędów;
+
+			panel.ColumnCount = liczbaKolumn;
+			panel.RowCount = liczbaRzędów;
+			for(int i=0; i<liczbaKolumn; i++)
+				panel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F / liczbaKolumn));
+			for(int i=0; i<liczbaRzędów; i++)
+				panel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F / liczbaRzędów));
+			panel.Dock = DockStyle.Fill;
+			panel.Margin = new Padding(0);
+
+			piksele = new PictureBox[liczbaKolumn, liczbaRzędów];
+			for (int kolumna = 0; kolumna < liczbaKolumn; kolumna++)
+				for (int rząd = 0; rząd < liczbaRzędów; rząd++)
+				{
+					PictureBox piksel = piksele[kolumna, rząd];
+					piksel = new PictureBox();
+					//((ISupportInitialize)(piksel)).BeginInit();
+					panel.Controls.Add(piksel, kolumna, rząd);
+					piksel.Dock = DockStyle.Fill;
+					piksel.Image = global::familiada.Properties.Resources.żółty;
+					piksel.SizeMode = PictureBoxSizeMode.StretchImage;
+					piksel.Margin = new Padding(0);
+					//((ISupportInitialize)(piksel)).EndInit();
+				}
+		}
+
+		public void dodajDo(TableLayoutPanel panel, int kolumna, int rząd)
+		{
+			panel.Controls.Add(this.panel, kolumna, rząd);
+		}
+	}
+}
